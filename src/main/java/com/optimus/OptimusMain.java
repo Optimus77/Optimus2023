2d1e96a4a5cca85adebca686d37a07dfe2a0548c
“Optimus77” <Optimus_7@163.com>
2023-07-14 17:48:24 +0800

    init

---
+++ src/main/java/com/optimus/OptimusMain.java
@@ -0,0 +1,24 @@
+package com.optimus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot启动类,定义了一个"/"访问接口
+ */
+@RestController
+@SpringBootApplication
+public class OptimusMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimusMain.class, args);
+    }
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello Spring Boot!";
+    }
+
+}
